12639746f82aa8ddf0ae7f2d5efc72d90dbf5513
Luciano <lulopeslulopes@yahoo.com>
2024-05-08 17:13:52 -0300

    Exercicios com erros propositais e algumas dificuldades de gerar o arquivo web para exemplificação.

---
+++ calculadora/Calculadora/src/Comentarios.java
@@ -0,0 +1,48 @@
+public class Comentarios {
+    /**
+     * @param args
+     * @return
+     */
+    public static int main(String[] args) {
+        //Comentário em apenas uma linha
+        /*
+         * Comentário de várias linhas
+         */
+
+
+
+        /**
+         * Isso é uma documentação
+         * Isso é uma documentação
+         * Isso é uma documentação
+         * Isso é uma documentação
+         * Isso é uma documentação
+         * Isso é uma documentação
+         * Isso é uma documentação
+         */
+
+
+
+/* * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+*/          
+
+public void somaMultiplica(){
+    int r = 0; 
+    int x = 0;
+    int n = 0;
+    // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x
+    }
+    return r;
+}
+    }
+
+ 
+}
